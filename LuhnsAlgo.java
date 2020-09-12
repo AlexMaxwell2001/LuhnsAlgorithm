3a9ea59e63429a74b92ecbd9039616174566b61e
Alex Maxwell <66850447+AlexMaxwell2001@users.noreply.github.com>
2020-09-12 21:23:36 +0100

    Add files via upload

---
+++ LuhnsAlgo.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class LuhnsAlgo 
+{
+    public static void main(String args[]) throws Exception 
+    {
+        Scanner sc = new Scanner(System.in);
+        String cardNumber = sc.nextLine();
+        int sum = 0;
+        for(int j=input.length()-1;j>=0;j--)
+        {
+            int number=Integer.parseInt(""+cardNumber.charAt(j));
+            if((input.length()-1-j)%2==1){
+                number=number*2;
+                if(number>9){
+                    number=number-9;
+                }
+            }
+            sum=sum+number;
+        }
+        if(sum%10==0){
+            System.out.println("Card Number is Valid!");
+        }
+        else{
+            System.out.println("This Card is a Fake!");
+        }
+    }
+}
